06c95fb9fce90d3319e219cf1d5c89508968f069
Marko Gargenta <marko@marakana.com>
2013-02-05 11:16:21 -0800

    Added onClick() support

--- Yamba/src/com/cisco/yamba/StatusActivity.java
+++ Yamba/src/com/cisco/yamba/StatusActivity.java
@@ -2,13 +2,30 @@ package com.cisco.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
+	EditText editStatus;
+	Button buttonUpdate;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_status);
-    }
-    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_status);
+
+		editStatus = (EditText) findViewById(R.id.text_status);
+		buttonUpdate = (Button) findViewById(R.id.button_update);
+
+		buttonUpdate.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		Log.d("Yamba", "onClick'd");
+	}
+
 }
