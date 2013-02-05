e680b8e09870f5e6f21bc35fcbd160209d37c445
Marko Gargenta <marko@marakana.com>
2013-02-05 13:08:16 -0800

    Implemented PostStatus AsyncTask

--- Yamba/src/com/cisco/yamba/StatusActivity.java
+++ Yamba/src/com/cisco/yamba/StatusActivity.java
@@ -1,12 +1,17 @@
 package com.cisco.yamba;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClientException;
 
 public class StatusActivity extends Activity implements OnClickListener {
 	EditText editStatus;
@@ -25,8 +30,35 @@ public class StatusActivity extends Activity implements OnClickListener {
 
 	@Override
 	public void onClick(View v) {
+
+		// Post to cloud
 		String status = editStatus.getText().toString();
-		Log.d("Yamba", "onClick'd with text: " + status);
+		new PostStatusTask().execute(status);
+		
 	}
 
+	private class PostStatusTask extends AsyncTask<String, Void, String> {
+
+		/** Executes on a worker thread */
+		@Override
+		protected String doInBackground(String... param) {
+			try {
+				YambaClient yamba = new YambaClient("student", "password");
+				yamba.postStatus(param[0]);
+				return "Successfully posted";
+			} catch (YambaClientException e) {
+				Log.e("Yamba", "Failed to post", e);
+				e.printStackTrace();
+				return "Failed to post";
+			}
+		}
+
+		/** Executes on the UI thread */
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}
+
+		
+	}
 }
