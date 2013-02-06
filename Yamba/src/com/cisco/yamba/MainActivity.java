d495e64258f453574af2015d79e1b6d7177e16d2
Marko Gargenta <marko@marakana.com>
2013-02-05 17:26:27 -0800

    Added options menu

--- Yamba/src/com/cisco/yamba/MainActivity.java
+++ Yamba/src/com/cisco/yamba/MainActivity.java
@@ -2,6 +2,7 @@ package com.cisco.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +10,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
 	}
 
 	
