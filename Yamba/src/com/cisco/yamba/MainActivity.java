fe9f07c73e7d0f9231f01b2ef5196d6470ddd0f9
Marko Gargenta <marko@marakana.com>
2013-02-06 11:22:24 -0800

    Added Status Intent

--- Yamba/src/com/cisco/yamba/MainActivity.java
+++ Yamba/src/com/cisco/yamba/MainActivity.java
@@ -1,8 +1,10 @@
 package com.cisco.yamba;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -16,6 +18,18 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case R.id.item_status:
+			startActivity( new Intent(this, StatusActivity.class) );
+			
+			return true;
+			default:
+				return false;
+		}
 	}
 
 	
--- Yamba/src/com/cisco/yamba/StatusFragment.java
+++ Yamba/src/com/cisco/yamba/StatusFragment.java
@@ -11,7 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
