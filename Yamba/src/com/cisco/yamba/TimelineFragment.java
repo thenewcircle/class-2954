d885076c880ac110ca152717d4efc183bca218dd
Marko Gargenta <marko@marakana.com>
2013-02-08 10:48:51 -0800

    Implemented CursorLoader

--- Yamba/src/com/cisco/yamba/TimelineFragment.java
+++ Yamba/src/com/cisco/yamba/TimelineFragment.java
@@ -1,29 +1,57 @@
 package com.cisco.yamba;
 
 import android.app.ListFragment;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CursorAdapter;
 import android.widget.SimpleCursorAdapter;
 
-public class TimelineFragment extends ListFragment {
+public class TimelineFragment extends ListFragment implements
+		LoaderCallbacks<Cursor> {
+	private static final String TAG = "Yamba";
 	private static final String[] FROM = { StatusContract.Columns.USER,
 			StatusContract.Columns.MESSAGE };
 	private static final int[] TO = { android.R.id.text1, android.R.id.text2 };
+	private static final int LOADER_ID = 47;
+	private SimpleCursorAdapter adapter;
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 
-		Cursor cursor = getActivity().getContentResolver().query(
-				StatusContract.CONTENT_URI, null, null, null,
-				StatusContract.DEFAULT_SORT);
-
-		SimpleCursorAdapter adapter = new SimpleCursorAdapter(getActivity(),
-				android.R.layout.simple_list_item_2, cursor, FROM, TO,
-				CursorAdapter.FLAG_AUTO_REQUERY);
+		adapter = new SimpleCursorAdapter(getActivity(),
+				android.R.layout.simple_list_item_2, null, FROM, TO, 0);
 
 		setListAdapter(adapter);
+
+		getLoaderManager().initLoader(LOADER_ID, null, this);
+	}
+
+	// -- Loader Callbacks ---
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		Log.d(TAG, "onCreateLoader");
+		if (id != LOADER_ID)
+			return null; // Assert
+		return new CursorLoader(getActivity(), StatusContract.CONTENT_URI,
+				null, null, null, StatusContract.DEFAULT_SORT);
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		Log.d(TAG, "onLoadFinished");
+		adapter.swapCursor(cursor);
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> laoder) {
+		Log.d(TAG, "onLoaderReset");
+		adapter.swapCursor(null);
 	}
 
 }
