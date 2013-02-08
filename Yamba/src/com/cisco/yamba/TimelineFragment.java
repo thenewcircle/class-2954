35018703e9f2fdea1ecdf2669afb8b87f4942f29
Marko Gargenta <marko@marakana.com>
2013-02-08 11:25:27 -0800

    Implemented ViewBinder

--- Yamba/src/com/cisco/yamba/TimelineFragment.java
+++ Yamba/src/com/cisco/yamba/TimelineFragment.java
@@ -6,16 +6,20 @@ import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.util.Log;
-import android.widget.CursorAdapter;
+import android.view.View;
 import android.widget.SimpleCursorAdapter;
+import android.widget.SimpleCursorAdapter.ViewBinder;
+import android.widget.TextView;
 
 public class TimelineFragment extends ListFragment implements
 		LoaderCallbacks<Cursor> {
 	private static final String TAG = "Yamba";
 	private static final String[] FROM = { StatusContract.Columns.USER,
-			StatusContract.Columns.MESSAGE };
-	private static final int[] TO = { android.R.id.text1, android.R.id.text2 };
+			StatusContract.Columns.MESSAGE, StatusContract.Columns.CREATED_AT };
+	private static final int[] TO = { R.id.text_user, R.id.text_message,
+			R.id.text_created_at };
 	private static final int LOADER_ID = 47;
 	private SimpleCursorAdapter adapter;
 
@@ -23,13 +27,31 @@ public class TimelineFragment extends ListFragment implements
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 
-		adapter = new SimpleCursorAdapter(getActivity(),
-				android.R.layout.simple_list_item_2, null, FROM, TO, 0);
+		setEmptyText("Loading the data...");
+
+		adapter = new SimpleCursorAdapter(getActivity(), R.layout.row, null,
+				FROM, TO, 0);
+		adapter.setViewBinder(VIEW_BINDER);
 
 		setListAdapter(adapter);
 
 		getLoaderManager().initLoader(LOADER_ID, null, this);
 	}
+
+	private static final ViewBinder VIEW_BINDER = new ViewBinder() {
+
+		@Override
+		public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+			if (view.getId() != R.id.text_created_at)
+				return false;
+			
+			long timestamp = cursor.getLong(columnIndex);
+			CharSequence relTime = DateUtils.getRelativeTimeSpanString(timestamp);
+			((TextView)view).setText(relTime);
+			
+			return true;
+		}
+	};
 
 	// -- Loader Callbacks ---
 
