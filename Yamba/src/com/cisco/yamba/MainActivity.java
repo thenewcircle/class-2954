45aa0655996e4bb2122291b04977251ef2865f0e
Marko Gargenta <marko@marakana.com>
2013-02-08 15:42:58 -0800

    Implemented FragmentManager

--- Yamba/src/com/cisco/yamba/MainActivity.java
+++ Yamba/src/com/cisco/yamba/MainActivity.java
@@ -1,8 +1,10 @@
 package com.cisco.yamba;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
@@ -17,9 +19,29 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
+	private static MenuItem statusItem;
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
+		statusItem = menu.findItem(R.id.item_status);
+		return true;
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		if (statusItem == null)
+			return true;
+
+		Fragment frag = getFragmentManager().findFragmentById(R.id.fragment_status);
+		if (frag!=null && frag.isAdded()) {
+			Log.d("Yamba", "onPrepareOptionsMenu false");
+			statusItem.setVisible(false);
+		} else {
+			Log.d("Yamba", "onPrepareOptionsMenu true");
+			statusItem.setVisible(true);
+		}
+
 		return true;
 	}
 
