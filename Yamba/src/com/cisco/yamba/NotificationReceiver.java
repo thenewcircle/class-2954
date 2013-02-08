3061af35f3fca60db79d30c1291df91359595c10
Marko Gargenta <marko@marakana.com>
2013-02-08 12:24:57 -0800

    Added NotificationReceiver scaffolding

---
+++ Yamba/src/com/cisco/yamba/NotificationReceiver.java
@@ -0,0 +1,16 @@
+package com.cisco.yamba;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+
+public class NotificationReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.d("NotificationReceiver", "onReceive");
+	}
+
+}
