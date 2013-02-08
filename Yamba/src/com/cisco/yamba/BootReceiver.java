40ac76bf382dc850f4d3a90ef79d5ff4b068b93d
Marko Gargenta <marko@marakana.com>
2013-02-08 13:07:17 -0800

    Implemented Alarms

--- Yamba/src/com/cisco/yamba/BootReceiver.java
+++ Yamba/src/com/cisco/yamba/BootReceiver.java
@@ -1,16 +1,25 @@
 package com.cisco.yamba;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
 public class BootReceiver extends BroadcastReceiver {
+	private static final long INTERVAL = 30 * 1000L;
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Log.d("BootReceiver", "onReceive: "+ System.currentTimeMillis() );
-		context.startService( new Intent(context, RefreshService.class) );
-	}
+		Log.d("BootReceiver", "onReceive: " + System.currentTimeMillis());
 
+		PendingIntent operation = PendingIntent.getService(context, 47,
+				new Intent(context, RefreshService.class), PendingIntent.FLAG_UPDATE_CURRENT);
+		
+		AlarmManager alarmManager = (AlarmManager) context
+				.getSystemService(Context.ALARM_SERVICE);
+		alarmManager.setInexactRepeating(AlarmManager.RTC,
+				System.currentTimeMillis(), INTERVAL, operation);
+	}
 }
